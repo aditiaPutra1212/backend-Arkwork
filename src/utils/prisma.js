d8a5a4d64352136c7f44c5f0da132b8ddd476fea
aditiaputra1212 <aditiaputra88890@gmail.com>
2025-08-29 01:16:01 +0700

    first commit

--- src/utils/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-// backend/src/utils/prisma.ts
-const client_1 = require("@prisma/client");
-exports.prisma = new client_1.PrismaClient();
-process.on('beforeExit', async () => {
-    await exports.prisma.$disconnect();
-});
